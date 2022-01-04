d87d508718b580df301902571c79b155a3a34723
Theo Vora <theo.vora@gmail.com>
2022-01-04 14:08:21 -0700

    Add todo to make bars more dramatic in scale

--- Shared/ChartData.swift
+++ Shared/ChartData.swift
@@ -13,11 +13,23 @@ struct ChartData {
 }
 
 let chartDataSet = [
-    ChartData(label: "Mon", value: 3),
-    ChartData(label: "Tue", value: 4),
-    ChartData(label: "Wed", value: 5),
-    ChartData(label: "Thu", value: 2),
-    ChartData(label: "Fri", value: 7),
-    ChartData(label: "Sat", value: 10),
-    ChartData(label: "Sun", value: 9)
+    ChartData(label: "Mon", value: 221014),
+    ChartData(label: "Tue", value: 221014),
+    ChartData(label: "Wed", value: 221014),
+    ChartData(label: "Thu", value: 221014),
+    ChartData(label: "Fri", value: 222912),
+    ChartData(label: "Sat", value: 223678),
+    ChartData(label: "Sun", value: 225040)
 ]
+
+// TODO: Make the bars more dramatic by eliminating first 2 significant digits
+//
+//let chartDataSet = [
+//    ChartData(label: "Mon", value: 1014),
+//    ChartData(label: "Tue", value: 1014),
+//    ChartData(label: "Wed", value: 1014),
+//    ChartData(label: "Thu", value: 1014),
+//    ChartData(label: "Fri", value: 2912),
+//    ChartData(label: "Sat", value: 3678),
+//    ChartData(label: "Sun", value: 5040)
+//]
