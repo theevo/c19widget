a8d0b03bfa6bfcecf571cab8116bdd93dfa19f16
Theo Vora <theo.vora@gmail.com>
2022-01-04 10:41:08 -0700

    Fetch data from API

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,14 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var c19Data: [ChartData] = chartDataSet
+    
     var body: some View {
         VStack {
             BarChart(title: "Confirmed COVID-19 cases",
                  city: "Salt Lake City, Utah",
                  barColor: .blue,
-                 data: chartDataSet)
+                 data: c19Data)
             Text("Last updated: 12/30/2021 3:10 PM")
                 .font(.caption2)
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    private func loadData() async {
+        guard let url = URL(string: "https://api.covid19api.com/country/united-states/status/confirmed?from=2021-12-23T00:00:00Z&to=2021-12-29T00:00:00Z&province=Utah") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            do {
+                let decodedResponse = try JSONDecoder().decode([Covid19APIByCountryResponse].self, from: data)
+                
+                print("Got data")
+//                print(decodedResponse)
+            } catch DecodingError.keyNotFound(let key, let context) {
+                Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
+            } catch DecodingError.valueNotFound(let type, let context) {
+                Swift.print("could not find type \(type) in JSON: \(context.debugDescription)")
+            } catch DecodingError.typeMismatch(let type, let context) {
+                Swift.print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
+            } catch DecodingError.dataCorrupted(let context) {
+                Swift.print("data found to be corrupted in JSON: \(context.debugDescription)")
+            } catch let error as NSError {
+                NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
+            }
+        } catch {
+            print("Invalid data")
         }
     }
 }
--- Shared/Covid19APIByCountryResponse.swift
+++ Shared/Covid19APIByCountryResponse.swift
@@ -25,18 +25,26 @@ import Foundation
 
 struct Covid19APIByCountryResponse: Codable {
     var country: String
+    var countryCode: String
     var province: String
     var city: String
+    var cityCode: String
+    var lat: String
+    var lon: String
     var cases: Int
     var status: String
-    var date: Date
+//    var date: String
     
     enum CodingKeys: String, CodingKey {
         case country = "Country"
+        case countryCode = "CountryCode"
         case province = "Province"
         case city = "City"
+        case cityCode = "CityCode"
+        case lat = "Lat"
+        case lon = "Lon"
         case cases = "Cases"
         case status = "Status"
-        case date = "Date"
+//        case date = "Date"
     }
 }
