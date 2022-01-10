fef2d7f9addd0425ab4024a848e763bc34b02e6c
Theo Vora <theo.vora@gmail.com>
2022-01-10 06:19:24 -0700

    Remove elements in View that are now owned by ViewModel

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,10 +13,6 @@ struct ContentView: View {
     
     @StateObject private var locationManager = LocationManager()
     
-    @State private var location: String = "Salt Lake City, Utah"
-    @State private var c19Data: [ChartData] = chartDataSet
-    @State private var updatedTime: String = ""
-    
     var body: some View {
         VStack {
             BarChart(title: "Confirmed COVID-19 cases",
@@ -27,15 +23,6 @@ struct ContentView: View {
                 .font(.caption2)
         }
         .onAppear(perform: viewModel.update)
-    }
-    
-    private func loadData() async {
-        if let data = await Covid19APIByCountry.getData() {
-            
-            c19Data = data.0
-            location = Covid19APIByCountry.city + ", " + Covid19APIByCountry.province
-            updatedTime = "Last updated: " + data.1.covid19WidgetDateTime
-        }
     }
 }
 
